065e0176d2e49830c32b181325c00c4a84513a90
affffffffffffffffffffff <86025943+affffffffffffffffffffff@users.noreply.github.com>
2023-03-23 12:37:52 -0500

    Add files via upload

--- Cycle 5/sketch_mar22a/sketch_mar22a.ino
+++ Cycle 5/sketch_mar22a/sketch_mar22a.ino
@@ -1,5 +1,5 @@
 /* Video Link:
- * https://youtu.be/jf56VGvDk4I
+ * https://youtu.be/6N80SaPqaUg
  */
 
 #define ENABLE 5
@@ -18,58 +18,30 @@ void setup() {
 void loop() {
   digitalWrite(ENABLE, HIGH);
   Serial.println("spin up clockwise"); //spinup cw
-  for (int i = 0; i < 75; i++) {
+  for (int i = 0; i < 256; i++) {
     analogWrite(DIRA, i);
-    delay(150);
+    delay(15);
   }    
-  analogWrite(DIRA, 150);
-  delay(50);
-  analogWrite(DIRA, 75);
-  delay(1000); //inertia
-  for (int i = 125; i < 256; i++) {
-    analogWrite(DIRA, i);
-    delay(20);
-  }
   Serial.println("max clockwise speed reached");
   delay(2000); 
   Serial.println("spin down clockwise"); //spinup cw
-  for (int i = 255; i > 70; i--) {
+  for (int i = 255; i >= 0; i--) {
     analogWrite(DIRA, i);
-    delay(20);
-  }    
-  analogWrite(DIRA, 70);
-  delay(1000);
-  for (int i = 70; i > 0; i--) {
-    analogWrite(DIRA, i);
-    delay(100);
-  }  
-  digitalWrite(DIRA, LOW);
-  Serial.println("spin up counter-clockwise"); //spinup ccw
-  for (int i = 0; i < 75; i++) {
-    analogWrite(DIRB, i);
-    delay(150);
-  }    
-  analogWrite(DIRB, 150);
-  delay(50);
-  analogWrite(DIRB, 75);
-  delay(1000); //inertia
-  for (int i = 125; i < 256; i++) {
-    analogWrite(DIRB, i);
     delay(20);
   }
+  delay(1000);
+  Serial.println("spin up counter-clockwise"); //spinup ccw
+  for (int i = 0; i < 256; i++) {
+    analogWrite(DIRB, i);
+    delay(15);
+  }   
   Serial.println("max counter-clockwise speed reached");
   delay(2000);
   Serial.println("spin doiwn counter-clockwise"); //spindown ccw
-  for (int i = 255; i > 70; i--) {
+  for (int i = 255; i >= 0; i--) {
     analogWrite(DIRB, i);
     delay(20);
-  }    
-  analogWrite(DIRB, 70);
+  }
   delay(1000);
-  for (int i = 70; i > 0; i--) {
-    analogWrite(DIRB, i);
-    delay(100);
-  }    
-  digitalWrite(DIRB, LOW);
 }
    
